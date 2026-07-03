8b8c7cd8d2e19b4956ca51dd0689716c52c8cf37
yigeyouzia <1793402316@qq.com>
2026-07-03 15:53:22 +0800

    loss 时间

---
+++ plot_utils.py
@@ -0,0 +1,42 @@
+"""训练损失可视化工具
+
+Usage: 在 notebook 训练 cell 末尾加一行即可：
+
+    from plot_utils import plot_loss
+    plot_loss(loss_track, save_path='./loss_curve.png')
+
+参数:
+    loss_track : list of [loss_res, loss_bc, loss_ic] 或 [loss_res, loss_bc, loss_ic, ...]
+    save_path  : 保存路径，默认 './loss_curve.png'
+    title      : 图标题，默认 'Training Loss'
+    labels     : 自定义标签列表，默认 ['Loss_Res', 'Loss_BC', 'Loss_IC', ...]
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_loss(loss_track, save_path='./loss_curve.png', title='Training Loss',
+              labels=None):
+    loss_track = np.array(loss_track)
+    epochs = np.arange(1, len(loss_track) + 1)
+
+    n = loss_track.shape[1]
+    if labels is None:
+        default_labels = ['Loss_Res', 'Loss_BC', 'Loss_IC',
+                          'Loss_IC2', 'Loss_Data']
+        labels = default_labels[:n]
+
+    plt.figure(figsize=(6, 4))
+    for i in range(n):
+        plt.plot(epochs, loss_track[:, i], label=labels[i], linewidth=1)
+
+    plt.yscale('log')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title(title)
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.tight_layout()
+    plt.savefig(save_path, dpi=150)
+    plt.show()
+    print(f'Loss curve saved to {save_path}')
